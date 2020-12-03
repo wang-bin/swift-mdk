1461c24f231d0726ce67447b5bd53255b83af3f1
wang-bin <wbsecg1@gmail.com>
2020-12-03 18:24:15 +0800

    more player apis except rare used ones

--- Player.swift
+++ Player.swift
@@ -4,133 +4,18 @@
 //
 //  Created by WangBin on 2020/12/1.
 //
-
+#if canImport(mdk)
 import mdk
+#endif
 import MetalKit
 // https://stackoverflow.com/questions/43880839/swift-unable-to-cast-function-pointer-to-void-for-use-in-c-style-third-party
 // https://stackoverflow.com/questions/37401959/how-can-i-get-the-memory-address-of-a-value-type-or-a-custom-struct-in-swift
 // https://stackoverflow.com/questions/33294620/how-to-cast-self-to-unsafemutablepointervoid-type-in-swift
 import Foundation // needed for strdup and free
 
-public enum MediaType : Int32 {
-    case Unknown = -1
-    case Video = 0
-    case Audio = 1
-    case Subtitle = 2
-}
-// TODO: RawOptionSet?
-public enum MediaStatus : UInt32 {
-    case NoMedia = 0
-    case Unloaded = 1
-    case Loading = 2
-    case Loaded = 4
-    case Prepared = 256
-    case Stalled = 8
-    case Buffering = 16
-    case Buffered = 32
-    case End = 64
-    case Seeking = 128
-    case Invalid = 0xffffffff
-}
-
-public enum State : UInt32 {
-    case Stopped = 0
-    case Playing = 1
-    case Paused = 2
-}
-
-public enum SeekFlag : UInt32 {
-    case From0 = 1
-    case FromStart = 2
-    case FromNow = 4
-    case KeyFrame = 256
-    case Default = 257
-}
-
-public enum VideoEffect : UInt32 {
-    case Brightness = 0
-    case Contrast = 1
-    case Hue = 2
-    case Saturation = 3
-}
-
-public enum LogLevel : UInt32 {
-    case Off = 0
-    case Error = 1
-    case Warning = 2
-    case Info = 3
-    case Debug = 4
-    case All = 5
-}
-
-public func version() ->Int32 {
-    return MDK_version()
-}
-
-public func setLogLevel(_ value : LogLevel) {
-    MDK_setLogLevel(MDK_LogLevel(value.rawValue))
-}
-
-public func logLevel() ->LogLevel {
-    return LogLevel(rawValue: MDK_logLevel().rawValue)!
-}
-
-public typealias LogHandler = (LogLevel,String)->Void
-public func setLogHandler(_ callback:LogHandler?) {
-    struct H {
-        static var cb : UnsafeMutableRawPointer? //LogHandler?
-    }
-    if H.cb == nil {
-        H.cb = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<LogHandler>.stride, alignment: MemoryLayout<LogHandler>.alignment)
-    }
-    var tmp = callback
-    H.cb?.initializeMemory(as: LogHandler.self, from: &tmp!, count: 1)
-    func _f(level : MDK_LogLevel, msg : UnsafePointer<CChar>?, opaque : UnsafeMutableRawPointer?) {
-        let f = opaque?.load(as: LogHandler.self)
-        f!(LogLevel(rawValue: level.rawValue)!, String(cString: msg!))
-    }
-    var h = mdkLogHandler()
-    if callback == nil {
-        h.opaque = nil
-    } else {
-        h.opaque = H.cb
-    }
-    h.cb = _f
-    MDK_setLogHandler(h)
-    // TODO: reset before H.cb destroyed
-}
-
-public func setGlobalOption<T>(name:String, value:T) {
-    //var ptr = UnsafePointer(&s.utf8CString)
-    if T.self == String.self {
-        (value as! String).withCString({
-            MDK_setGlobalOptionString(name, $0)
-        })
-    } else if T.self == Int32.self {
-        MDK_setGlobalOptionInt32(name, value as! Int32)
-    } else if T.self == Bool.self {
-        let v = Int32(value as! Bool ? 1 : 0)
-        MDK_setGlobalOptionInt32(name, v)
-    } else if T.self == Int.self {
-        let v = Int32(value as! Int)
-        MDK_setGlobalOptionInt32(name, v)
-    }
-}
-
-func declmetatype<T>(_: T) -> T.Type {
-    return T.self
-}
-// for char const* []
-func withArrayOfCStrings2<R>(
-    _ args: [String],
-    _ body: ([OpaquePointer?]) -> R
-) -> R {
-    var cStrings = args.map { OpaquePointer(strdup($0)) }
-    cStrings.append(nil)
-    defer {
-        cStrings.forEach { free(UnsafeMutablePointer<CChar>($0)) }
-    }
-    return body(cStrings)
+public enum MapDirection : UInt32 {
+    case FrameToViewport
+    case ViewportToFrame
 }
 
 // for char* const []
@@ -147,19 +32,22 @@ func withArrayOfCStrings<R>(
 }
 
 
-func bridge<T : AnyObject>(obj : T) -> UnsafeRawPointer {
-    return UnsafeRawPointer(Unmanaged.passUnretained(obj).toOpaque())
-    // return unsafeAddressOf(obj) // ***
+func bridge<T : AnyObject>(obj : T?) -> UnsafeRawPointer? {
+    guard let o = obj else {
+        return nil
+    }
+    return UnsafeRawPointer(Unmanaged.passUnretained(o).toOpaque())
 }
 
-func bridge<T : AnyObject>(obj : T) -> UnsafeMutableRawPointer {
-    return UnsafeMutableRawPointer(Unmanaged.passUnretained(obj).toOpaque())
-    // return unsafeAddressOf(obj) // ***
+func bridge<T : AnyObject>(obj : T?) -> UnsafeMutableRawPointer? {
+    guard let o = obj else {
+        return nil
+    }
+    return UnsafeMutableRawPointer(Unmanaged.passUnretained(o).toOpaque())
 }
 
 func bridge<T : AnyObject>(ptr : UnsafeRawPointer) -> T {
     return Unmanaged<T>.fromOpaque(ptr).takeUnretainedValue()
-    // return unsafeBitCast(ptr, T.self) // ***
 }
 
 func address(o: UnsafeRawPointer) -> OpaquePointer {
@@ -172,19 +60,19 @@ class Player {
             player.pointee.setMute(player.pointee.object, mute)
         }
     }
-    
+
     public var volume:Float = 1.0 {
         didSet {
             player.pointee.setVolume(player.pointee.object, volume)
         }
     }
-    
+
     public var media = "" {
         didSet {
             player.pointee.setMedia(player.pointee.object, media)
         }
     }
-    
+
     // audioDecoders
     public var audioDecoders = ["FFmpeg"] {
         didSet {
@@ -197,7 +85,7 @@ class Player {
             }
         }
     }
-    
+
     public var videoDecoders = ["FFmpeg"] {
         didSet {
             withArrayOfCStrings(videoDecoders) {
@@ -209,44 +97,56 @@ class Player {
             }
         }
     }
-    
+
     public var state:State = .Stopped {
         didSet {
             player.pointee.setState(player.pointee.object, MDK_State(state.rawValue))
         }
     }
-    
-    public var mediaStatus : MDK_MediaStatus {
+
+    public var mediaStatus : MediaStatus {
         get {
-            return player.pointee.mediaStatus(player.pointee.object)
+            return MediaStatus(rawValue: player.pointee.mediaStatus(player.pointee.object).rawValue)
         }
     }
-    
+
     public var loop:Int32 = 0 {
         didSet {
             player.pointee.setLoop(player.pointee.object, loop)
         }
     }
-    
+
     public var preloadImmediately = true {
         didSet {
             player.pointee.setPreloadImmediately(player.pointee.object, preloadImmediately)
         }
     }
-        
+
+    public var position : Int64 {
+        get {
+            player.pointee.position(player.pointee.object)
+        }
+    }
+
+    public var playbackRate : Float = 1.0 {
+        didSet {
+            player.pointee.setPlaybackRate(player.pointee.object, playbackRate)
+        }
+    }
+
     private var player : UnsafePointer<mdkPlayerAPI>! = mdkPlayerAPI_new()
-   
+
     deinit {
         mdkPlayerAPI_delete(&player)
         // TODO: deallocate callbacks?
     }
 
-    public func setRendAPI(_ api :  UnsafePointer<mdkMetalRenderAPI>) ->Void {
-        player.pointee.setRenderAPI(player.pointee.object, OpaquePointer(api), nil)
+    public func setRendAPI(_ api :  UnsafePointer<mdkMetalRenderAPI>, vid:AnyObject? = nil) ->Void {
+        player.pointee.setRenderAPI(player.pointee.object, OpaquePointer(api), bridge(obj: vid))
     }
 
-    // TODO: UIView, NSView, GLKView
-    public func setRenderTarget(_ mkv : MTKView, commandQueue cmdQueue: MTLCommandQueue) ->Void {
+    // TODO: UIView, NSView, GLKView. addRenderTarget, removeRenderTarget
+    public func setRenderTarget(_ mkv : MTKView, commandQueue cmdQueue: MTLCommandQueue, vid:AnyObject? = nil) ->Void {
         func currentRt(_ opaque: UnsafeRawPointer?)->UnsafeRawPointer? {
             guard let p = opaque else {
                 return nil
@@ -264,25 +164,25 @@ class Player {
         ra.cmdQueue = bridge(obj: cmdQueue)
         ra.opaque = bridge(obj: mkv)
         ra.currentRenderTarget = currentRt
-        setRendAPI(&ra)
+        setRendAPI(&ra, vid:vid)
     }
 
-    public func setVideoSurfaceSize(_ width : CGFloat, _ height : CGFloat)->Void {
-        player.pointee.setVideoSurfaceSize(player.pointee.object, Int32(width), Int32(height), nil)
+    public func setVideoSurfaceSize(_ width : CGFloat, _ height : CGFloat, vid:AnyObject? = nil)->Void {
+        player.pointee.setVideoSurfaceSize(player.pointee.object, Int32(width), Int32(height), bridge(obj: vid))
     }
 
-    public func renderVideo() -> Double {
-        return player.pointee.renderVideo(player.pointee.object, nil)
+    public func renderVideo(vid:AnyObject? = nil) -> Double {
+        return player.pointee.renderVideo(player.pointee.object, bridge(obj: vid))
     }
-    
+
     public func set(media:String, forType type:MediaType) {
         player.pointee.setMediaForType(player.pointee.object, media, MDK_MediaType(type.rawValue))
     }
-    
+
     public func setNext(media:String, from:Int64 = 0, withSeekFlag flag:SeekFlag = .Default) {
         player.pointee.setNextMedia(player.pointee.object, media, from, MDKSeekFlag(flag.rawValue))
     }
-    
+
     public func currentMediaChanged(_ callback:(()->Void)?) {
         func f_(opaque:UnsafeMutableRawPointer?) {
             let f = opaque?.load(as: (()->Void).self)
@@ -300,7 +200,7 @@ class Player {
         }
         player.pointee.currentMediaChanged(player.pointee.object, cb)
     }
-    
+
     public func setTimeout(_ value:Int64, callback:((Int64)->Bool)?) -> Void {
         typealias Callback = (Int64)->Bool
         func f_(value:Int64, opaque:UnsafeMutableRawPointer?)->Bool {
@@ -319,7 +219,7 @@ class Player {
         }
         player.pointee.setTimeout(player.pointee.object, value, cb)
     }
-    
+
     public func prepare(from:Int64, complete:((Int64, inout Bool)->Bool)?, _ flag:SeekFlag = .Default) {
         typealias Callback = (Int64, inout Bool)->Bool
         func _f(pos:Int64, boost:UnsafeMutablePointer<Bool>?, opaque:UnsafeMutableRawPointer?)->Bool {
@@ -342,7 +242,7 @@ class Player {
         cb.cb = _f
         player.pointee.prepare(player.pointee.object, from, cb, MDKSeekFlag(flag.rawValue))
     }
-    
+
     public func onStateChanged(callback:((State)->Void)?) -> Void {
         typealias Callback = (State)->Void
         func f_(state:MDK_State, opaque:UnsafeMutableRawPointer?)->Void {
@@ -361,16 +261,16 @@ class Player {
         }
         player.pointee.onStateChanged(player.pointee.object, cb)
     }
-    
-    public func waitFor(_ state:State, timeout:Int) -> Bool {
-        return player.pointee.waitFor(player.pointee.object, MDK_State(state.rawValue), timeout)
+
+    public func waitFor(_ state:State, timeout:Int? = -1) -> Bool {
+        return player.pointee.waitFor(player.pointee.object, MDK_State(state.rawValue), timeout ?? -1)
     }
-    
-    public func onMediaStatusChanged(callback:((MDK_MediaStatus)->Bool)?) {
-        typealias Callback = (MDK_MediaStatus)->Bool
+
+    public func onMediaStatusChanged(callback:((MediaStatus)->Bool)?) {
+        typealias Callback = (MediaStatus)->Bool
         func f_(status:MDK_MediaStatus, opaque:UnsafeMutableRawPointer?)->Bool {
             let f = opaque?.load(as: Callback.self)
-            return f!(status)
+            return f!(MediaStatus(rawValue: status.rawValue))
         }
         var cb = mdkMediaStatusChangedCallback()
         cb.cb = f_
@@ -384,10 +284,160 @@ class Player {
         }
         player.pointee.onMediaStatusChanged(player.pointee.object, cb)
     }
-    
+
+    public func setVideoSurfaceSize(_ width:Int32, _ height:Int32, vid:AnyObject? = nil) ->Void {
+        player.pointee.setVideoSurfaceSize(player.pointee.object, width, height, bridge(obj: vid))
+    }
+
+    /*!
+      \brief setVideoViewport
+      The rectangular viewport where the scene will be drawn relative to surface viewport.
+      x, y, width, height are normalized to [0, 1]
+    */
+    public func setVideoViewport(x:Float, y:Float, width:Float, height:Float, vid:AnyObject? = nil) ->Void {
+        player.pointee.setVideoViewport(player.pointee.object, x, y, width, height, bridge(obj: vid))
+    }
+
+    public func setAspectRatio(_ value:Float, vid:AnyObject? = nil) ->Void {
+        player.pointee.setAspectRatio(player.pointee.object, value, bridge(obj: vid))
+    }
+
+    public func mapPoint(_ dir:MapDirection, x:inout Float, y:inout Float, vid:AnyObject? = nil) -> Void {
+        player.pointee.mapPoint(player.pointee.object, MDK_MapDirection(dir.rawValue), &x, &y, nil, bridge(obj: vid))
+    }
+
+    public func rotate(_ degree:Int32, vid:AnyObject? = nil) -> Void {
+        player.pointee.rotate(player.pointee.object, degree, bridge(obj: vid))
+    }
+
+    public func scale(x:Float, y:Float, vid:AnyObject? = nil) -> Void {
+        player.pointee.scale(player.pointee.object, x, y, bridge(obj: vid))
+    }
+
+    public func setBackgroundColor(red:Float, green:Float, blue:Float, alpha:Float, vid:AnyObject? = nil) -> Void {
+        player.pointee.setBackgroundColor(player.pointee.object, red, green, blue, alpha, bridge(obj: vid))
+    }
+
+    public func set(effect:VideoEffect, values:[Float], vid:AnyObject? = nil) -> Void {
+        player.pointee.setVideoEffect(player.pointee.object, MDK_VideoEffect(effect.rawValue), values, bridge(obj: vid))
+    }
+
+    // TODO: vo_opaque
+    public func setRenderCallback(_ callback:(()->Void)?) -> Void {
+        typealias Callback = ()->Void
+        func f_(vo_opaque:UnsafeMutableRawPointer?, opaque:UnsafeMutableRawPointer?)->Void {
+            let f = opaque?.load(as: Callback.self)
+            return f!()
+        }
+        var cb = mdkRenderCallback()
+        cb.cb = f_
+        if callback != nil {
+            if render_cb_ == nil {
+                render_cb_ = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<Callback>.stride, alignment: MemoryLayout<Callback>.alignment)
+            }
+            cb.opaque = render_cb_
+            var tmp = callback
+            cb.opaque.initializeMemory(as: type(of: callback), from: &tmp, count: 1)
+        }
+        player.pointee.setRenderCallback(player.pointee.object, cb)
+    }
+
+    // TODO: onVideo, onAudio, beforeVideoRender, afterVideoRender
+
+    public func seek(_ pos:Int64, flags:SeekFlag, callback:((Int64)->Void)?) -> Bool {
+        typealias Callback = (Int64)->Void
+        func f_(ms:Int64, opaque:UnsafeMutableRawPointer?)->Void {
+            let f = opaque?.load(as: Callback.self)
+            return f!(ms)
+        }
+        var cb = mdkSeekCallback()
+        cb.cb = f_
+        if callback != nil {
+            if seek_cb_ == nil {
+                seek_cb_ = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<Callback>.stride, alignment: MemoryLayout<Callback>.alignment)
+            }
+            cb.opaque = seek_cb_
+            var tmp = callback
+            cb.opaque.initializeMemory(as: type(of: callback), from: &tmp, count: 1)
+        }
+        return player.pointee.seekWithFlags(player.pointee.object, pos, MDK_SeekFlag(rawValue: flags.rawValue), cb)
+    }
+
+    public func seek(_ pos:Int64, callback:((Int64)->Void)?) -> Bool {
+        return seek(pos, flags: .Default, callback: callback)
+    }
+
+    public func buffered(bytes:inout Int64) -> Int64 {
+        return player.pointee.buffered(player.pointee.object, &bytes)
+    }
+
+    public func buffered() -> Int64 {
+        return player.pointee.buffered(player.pointee.object, nil)
+    }
+
+    public func setBufferRange(msMin:Int64/* = 4000*/, msMax:Int64 = 16000, drop:Bool = false) -> Void {
+        player.pointee.setBufferRange(player.pointee.object, msMin, msMax, drop)
+    }
+
+    public func swithBitrate(url:String, delay:Int64 = -1, callback:((Bool)->Void)?) -> Void {
+        typealias Callback = (Bool)->Void
+        func f_(result:Bool, opaque:UnsafeMutableRawPointer?)->Void {
+            let f = opaque?.load(as: Callback.self)
+            return f!(result)
+        }
+        var cb = SwitchBitrateCallback()
+        cb.cb = f_
+        if callback != nil {
+            if switch_cb_ == nil {
+                switch_cb_ = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<Callback>.stride, alignment: MemoryLayout<Callback>.alignment)
+            }
+            cb.opaque = switch_cb_
+            var tmp = callback
+            cb.opaque.initializeMemory(as: type(of: callback), from: &tmp, count: 1)
+        }
+        player.pointee.switchBitrate(player.pointee.object, url, delay, cb)
+    }
+
+    // TODO: onEvent
+
+    public func record(to:String?, format:String?) -> Void {
+        player.pointee.record(player.pointee.object, to, format)
+    }
+    /*
+    func onLoop(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }*/
+
+    public func setRange(from msA:Int64, to msB:Int64 = -1) -> Void {
+        player.pointee.setRange(player.pointee.object, msA, msB)
+    }
+
+    public func onSync(_ callback:@escaping ()->Double, minInterval:Int32 = 10) -> Void {
+        typealias Callback = ()->Double
+        func f_(opaque:UnsafeMutableRawPointer?)->Double {
+            let f = opaque?.load(as: Callback.self)
+            return f!()
+        }
+        var cb = mdkSyncCallback()
+        cb.cb = f_
+        if sync_cb_ == nil {
+            sync_cb_ = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<Callback>.stride, alignment: MemoryLayout<Callback>.alignment)
+        }
+        cb.opaque = sync_cb_
+        var tmp = callback
+        cb.opaque.initializeMemory(as: type(of: callback), from: &tmp, count: 1)
+        player.pointee.onSync(player.pointee.object, cb, minInterval)
+    }
+
+    // TODO: updateNativeSurface
+
     private var prepare_cb : UnsafeMutableRawPointer? //((Int64, inout Bool)->Bool)?
     private var current_cb_ : UnsafeMutableRawPointer? // ()->Void
     private var timeout_cb_ : UnsafeMutableRawPointer? // (Int64)->Bool
     private var state_cb_ : UnsafeMutableRawPointer? // (State)->Void
-    private var status_cb_ : UnsafeMutableRawPointer? // (MDK_MediaStatus)->Bool
+    private var status_cb_ : UnsafeMutableRawPointer? // (MediaStatus)->Bool
+    private var render_cb_ : UnsafeMutableRawPointer? // ()->Void
+    private var seek_cb_ : UnsafeMutableRawPointer? // (Int64)->Void
+    private var switch_cb_ : UnsafeMutableRawPointer? // (Bool)->Void
+    private var sync_cb_ : UnsafeMutableRawPointer? // ()->Double
 }
