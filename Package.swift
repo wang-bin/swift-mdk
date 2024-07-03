6d4234c2876ca70c1163f6b4541d3de48a18f2c5
wang-bin <wbsecg1@gmail.com>
2024-07-03 14:34:25 +0800

    update sdk

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         //.binaryTarget(name: "mdk-sdk", path: "mdk-sdk/lib/mdk.xcframework"),
         .binaryTarget(name: "mdk-sdk"
-            , url: "https://github.com/wang-bin/mdk-sdk/releases/download/v0.28.0/mdk-sdk-apple.zip"
-            , checksum: "cdc7a63fb47d89f2bbfb91813a052d74492d94dce03a9eb3b60029724a129713"),
+            , url: "https://github.com/wang-bin/mdk-sdk/releases/download/v0.29.0/mdk-sdk-apple.zip"
+            , checksum: "77cd52e9c62a9b5fffc76e0b699e8a62291ea902ba3c44ac5481aa49fd51a0f7"),
         .target(
             name: "swift-mdk",
             dependencies: ["mdk-sdk"]),
